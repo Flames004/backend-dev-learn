ba60f6c3de9062e55e6cd1c7f6a278043c35bc90
Deepak Shukla <flamesofwisdom004@gmail.com>
2025-07-11 13:03:48 +0530

    created different routes using req url

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const http = require('http');
 
 const server = http.createServer((req,res)=>{
-    res.end("Hello World!");
+    if (req.url == '/about'){
+        res.end("About Page")
+    }
+    if (req.url == '/profile'){
+        res.end("Profile Page")
+    }
+    if (req.url == '/'){
+        res.end("Home Page")
+    }
+
 })
 
 server.listen(3000);

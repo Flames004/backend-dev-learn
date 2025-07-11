e4056e2273558ef50c3da7330730c6f56486246e
Deepak Shukla <flamesofwisdom004@gmail.com>
2025-07-11 13:12:03 +0530

    used express routes to get multiple responses using get method

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Hello World!");
+    res.send("Home Page");
+})
+app.get('/about', (req, res) => {
+    res.send("About Page");
+})
+app.get('/profile', (req, res) => {
+    res.send("Profile Page");
 })
 
 app.listen(3000);

eb6053bcfdf477c119637bf97478aaea68fbe162
Deepak Shukla <flamesofwisdom004@gmail.com>
2025-07-11 13:18:57 +0530

    using ejs to render html pages into express routes

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.set("view engine", 'ejs')   // to render html pages into express server
+
 app.get('/', (req, res) => {
-    res.send("Home Page");
+    res.render('index');    // render helps in rendering the page into the given route
 })
 app.get('/about', (req, res) => {
     res.send("About Page");

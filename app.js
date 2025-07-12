b0347fa66b91f75469fec3f05a7c9512d0c3cf49
Deepak Shukla <flamesofwisdom004@gmail.com>
2025-07-12 12:10:55 +0530

    using third party middleware

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
-
+const morgan = require('morgan');
 const app = express();
 
+app.use(morgan('dev'));  // to log the requests in the console
 app.set("view engine", 'ejs')   // to render html pages into express server
 
 // Middleware : the request pass through a function everytime it is rendered into the route or while changing the route
 // Types of middlewares : built in, custom, third party
-app.use((req,res,next)=>{   // to pass the request through a function called middleware
+app.use((req, res, next) => {   // to pass the request through a function called middleware
     console.log("This is middleware");
     // res.send("Middleware passed!")
     return next()   // to continue to the flow
